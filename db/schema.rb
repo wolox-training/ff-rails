908d2afe887d19405c600cc71e7cd7def6ccbabc
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-11-14 12:54:54 -0300

    PR Fixes

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 20191113124618) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "confirmation_token"
