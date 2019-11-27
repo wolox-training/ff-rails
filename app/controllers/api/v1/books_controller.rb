4b8305e0cb2e3b2a093912307e46cbe3f69dce53
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-11-27 12:12:42 -0300

    Code style fixes

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,13 +1,17 @@
-class Api::V1::BooksController < ApplicationController
-  include Wor::Paginate
+module Api
+  module V1
+    class BooksController < ApplicationController
+      include Wor::Paginate
 
-  before_action :authenticate_user! 
-  
-  def index
-    render_paginated Book.all
-  end
+      before_action :authenticate_user!
 
-  def show
-    render json: Book.find(params[:id])
+      def index
+        render_paginated Book.all
+      end
+
+      def show
+        render json: Book.find(params[:id])
+      end
+    end
   end
 end
