fa8d1ecc8875631b996a5ff73137f9d9f714423c
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-11-27 10:54:52 -0300

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-  has_many :rents, dependent: :destroy
+  has_one :rent, dependent: :destroy
   validates :first_name, :last_name, presence: true
 end
