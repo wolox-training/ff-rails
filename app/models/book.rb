ccf2d8f22fd8ef166450a367fbf798b1dcb93cbc
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-11-28 12:53:19 -0300

    Fixing relations with rents

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  has_one :rent, dependent: :destroy
+  has_many :rents, dependent: :destroy
   validates :genre, :author, :image, :title, :editor, :year, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-  has_one :rent, dependent: :destroy
+  has_many :rents, dependent: :destroy
   validates :first_name, :last_name, presence: true
 end
