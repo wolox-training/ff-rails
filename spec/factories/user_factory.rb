6514079a81cf7383fdabf237c9f3feef2b716b8f
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-03 12:48:30 -0300

    Fixing rubocop offenses

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     alphanumeric_password = Faker::Alphanumeric.alphanumeric 15
-  
+
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     password { alphanumeric_password }
