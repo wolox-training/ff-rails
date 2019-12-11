501b3caa8ae0e03e946e47af823cb0d1e5211f27
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-11 09:16:45 -0300

    Fixing missing stuff

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -6,7 +6,7 @@ module Api
       before_action :authenticate_user!
 
       def index
-        render_paginated Book.all
+        render_paginated Book.all, each_serializer: BookSerializer
       end
 
       def show
---
+++ app/serializers/rent_serializer.rb
@@ -0,0 +1,4 @@
+class RentSerializer < ActiveModel::Serializer
+  attributes :id, :start_date, :end_date, :book_id, :user_id
+end
+  
