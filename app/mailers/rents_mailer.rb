a280c36832b16c13263a57e88b303745c6c418ca
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-17 12:41:17 -0300

    Localization in emails

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -5,7 +5,8 @@ class RentsMailer < ApplicationMailer
     @rent = Rent.find(params[:rent_id])
     @user = @rent.user
     @book = @rent.book
+    @title = I18n.t 'rent_title'
 
-    mail(to: @user.email, subject: 'New rent')
+    mail(to: @user.email, subject: @title)
   end
 end
