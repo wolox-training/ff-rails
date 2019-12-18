f75210265f8d7830872c420aba8825d10337a311
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-18 12:20:03 -0300

    Removed unnecessary serializer calls

--- app/serializers/rent_serializer.rb
+++ app/serializers/rent_serializer.rb
@@ -1,5 +1,5 @@
 class RentSerializer < ActiveModel::Serializer
   attributes :id, :start_date, :end_date, :returned_at
-  has_one :book, serializer: BookSerializer
-  has_one :user, serializer: UserSerializer
+  has_one :book
+  has_one :user
 end
