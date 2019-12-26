2ab4be50c93e7809824f9cfdfa2372bf69c58179
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-26 11:47:30 -0300

    Added custom error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
   protect_from_forgery with: :null_session
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render json: { message: 'User not authorized to perform this action' }, status: :unauthorized
+  end
 end
--- app/policies/rent_policy.rb
+++ app/policies/rent_policy.rb
@@ -1,9 +1,4 @@
 class RentPolicy < ApplicationPolicy
-  def initialize(user, rent)
-    @user = user
-    @rent = rent
-  end
-
   class Scope
     def resolve
       scope.where(user: user)
