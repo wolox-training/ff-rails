a2eabedae177f3f3f252ab1cc9cc0b067efb4337
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-30 10:50:06 -0300

    Fixing CI

--- db/migrate/20191230131819_change_user_id_nullable_book_suggestion.rb
+++ db/migrate/20191230131819_change_user_id_nullable_book_suggestion.rb
@@ -1,5 +1,5 @@
 class ChangeUserIdNullableBookSuggestion < ActiveRecord::Migration[5.1]
   def change
-    change_column :book_suggestions, :user_id, null: true
+    change_column :book_suggestions, :user_id, :bigint, null: true
   end
 end
