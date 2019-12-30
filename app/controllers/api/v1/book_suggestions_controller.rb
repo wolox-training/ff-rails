5c18e8ed5ee7cd21a601170aca3b4d359c663b90
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-12-30 10:33:46 -0300

    Fixing rubocop offenses

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -1,15 +1,14 @@
 module Api
   module V1
     class BookSuggestionsController < ApiController
+      skip_before_action :authenticate_user!
       def create
         @book_suggestion = BookSuggestion.create(suggestion_params)
-        if current_user
-          @book_suggestion.user_id = current_user.id
-        end
+        @book_suggestion.user_id = current_user.id if current_user
         if @book_suggestion.save
           render json: @book_suggestion, status: :created
         else
-          render json: { message: 'Cannot save that suggestion, invalid data' }, status: :bad_request
+          render json: { message: 'Cannot save that suggestion' }, status: :bad_request
         end
       end
 
