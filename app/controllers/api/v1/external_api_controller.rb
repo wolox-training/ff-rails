125e0ffabfbbb4ada1a1b8bb6ffb2ccb26264421
Federico Fasitella <federico.fasitella@wolox.com.ar>
2020-01-08 12:23:54 -0300

    Actually fixing the rubocop issues

--- app/controllers/api/v1/external_api_controller.rb
+++ app/controllers/api/v1/external_api_controller.rb
@@ -4,9 +4,19 @@ module Api
       def show
         external_api_book = api_request
         book = external_api_book['ISBN:' + isbn]
+        return book_not_found if book.blank?
+
+        book_info(book)
+      end
+
+      def book_info(book)
         render json: book
           .slice('title', 'subtitle', 'number_of_pages', 'authors')
-          .merge(ISBN: isbn) if book.present?
+          .merge(ISBN: isbn)
+      end
+
+      def book_not_found
+        render json: { message: 'Book not found' }, status: :not_found
       end
 
       def api_request
