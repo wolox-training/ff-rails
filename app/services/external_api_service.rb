492786276f6b753168ad05f0df3ece8aa16cadd2
Federico Fasitella <federico.fasitella@wolox.com.ar>
2020-01-08 09:03:20 -0300

    Rubocop fixes

--- app/services/external_api_service.rb
+++ app/services/external_api_service.rb
@@ -1,9 +1,8 @@
 class ExternalApiService
-	include HTTParty
-	base_uri 'https://openlibrary.org/api/books?bibkeys=ISBN:'
+  include HTTParty
+  base_uri 'https://openlibrary.org/api/books?bibkeys=ISBN:'
 
-	def self.api_request(isbn)
-		self.get(base_uri + isbn + '&jscmd=data&format=json')
-	end
-
+  def self.api_request(isbn)
+    get(base_uri + isbn + '&jscmd=data&format=json')
+  end
 end
