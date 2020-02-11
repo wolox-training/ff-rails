14cc9e5325d42dad2633ff2b26628d278aad7193
Federico Fasitella <federico.fasitella@wolox.com.ar>
2020-02-11 11:30:20 -0300

    Adding book seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50000.times { Book.create(
+  genre: Faker::Book.genre,
+  author: Faker::Book.author,
+  image: Faker::Internet.url(host: 'wolox.com.ar', path: '/image.jpg'),
+  title: Faker::Book.title,
+  editor: Faker::Book.publisher ,
+  year: Faker::Number.between(from: 0, to: 2019) 
+  ) }
