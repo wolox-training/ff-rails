80d5b24554fdb7f6c0be915ae030c63cccc1906e
Federico Fasitella <federico.fasitella@wolox.com.ar>
2020-02-11 11:36:15 -0300

    Solving various issues in the project

--- spec/models/book_suggestion_spec.rb
+++ spec/models/book_suggestion_spec.rb
@@ -14,15 +14,4 @@ describe BookSuggestion do
     it { is_expected.to validate_presence_of(:year) }
     it { is_expected.to be_persisted }
   end
-
-  context 'invalid model' do
-    let!(:user) { create(:user) }
-    %i[link author image title editor year].each do |field|
-      subject(:book_suggestion) { build(:book_suggestion, user: user, field => nil) }
-      it { is_expected.not_to be_valid }
-      it 'is not persisted' do
-        expect(subject.save).to be false
-      end
-    end
-  end
 end
