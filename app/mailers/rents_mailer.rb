a4d0017cdc4988a2cc5c366d9cdf678271af7e7c
Federico Fasitella <federico.fasitella@wolox.com.ar>
2020-02-17 09:42:16 -0300

    Fixing localization

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -3,22 +3,22 @@ class RentsMailer < ApplicationMailer
     @rent = Rent.find(params[:rent_id])
     @user = @rent.user
     @book = @rent.book
-    @title = t('rents_mailer.creation_email.rent_title')
 
     I18n.with_locale(@user.locale) do
+      @title = t('rents_mailer.creation_email.rent_title')
       mail(to: @user.email, subject: @title)
     end
   end
 
   def rent_end_email
     @rents = Rent.ending_today
-    @title = t('rents_mailer.rent_end_email.title')
+    @rents.each do |rent|
+      @rent = rent
+      @user = rent.user
+      @book = rent.book
 
-    @rents.each do |r|
-      @rent = r
-      @user = r.user
-      @book = r.book
       I18n.with_locale(@user.locale) do
+        @title = t('rents_mailer.rent_end_email.title')
         mail(to: @user.email, subject: @title)
       end
     end
